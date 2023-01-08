edbb11ad0ec6c72665952d5e318d6f3ba7108932
JohnNarofsky <john.narofsky@gmail.com>
2023-01-08 12:45:16 -0600

    latest

--- src/resume/resume.js
+++ src/resume/resume.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './resume.css';
+import Education from './education/education'
+import Identification from './identification/identification'
+import Skills from './skills/skills';
+import Experience from './experience/experience';
+
 
 export default class Resume extends React.Component{
     render() {
         return (
-            <div>Resume</div>
+            <>
+                <div>Resume</div>
+                <div><Identification/></div>
+                <div><Education/></div>
+                <div><Skills/></div>
+                <div><Experience/></div>
+            </>
         );
     }
 }
